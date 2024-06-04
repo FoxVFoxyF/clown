83442e7ca62f80a9e6d31e9e298ea7c90e4150fd
FoxVFoxy <121633580+FoxVFoxyF@users.noreply.github.com>
2024-06-04 10:00:20 +0200

    reduced the time

--- runner.bat
+++ runner.bat
@@ -3,8 +3,8 @@ echo loading...
 
 start cmd /k "node characterai_server.js"
 
-timeout /t 10 /nobreak
+timeout /t 8 /nobreak
 
-start cmd /k "python clown_0.2.py"
+start cmd /k "python clown_0.3.py"
 
 @pause
