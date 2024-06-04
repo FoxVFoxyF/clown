debb8c63e11ef1871f830e09d16b8aa7b5e92f40
FoxVFoxy <121633580+FoxVFoxyF@users.noreply.github.com>
2024-06-04 13:19:22 +0200

    Update clown_0.3.py

--- clown_0.3.py
+++ clown_0.3.py
@@ -11,7 +11,7 @@ token = "YOUR TOKEN HERE"
 channel_id = "CHANNEL ID"
 username_to_ignore = 'bigwhiteguy' # your @ bot
 check_for_prefix = True # check that selected prefix will be checked
-prefix = "." # example: if user send msg like .hello! program will send this to c.ai but if user type hello he will be ignored
+prefix = "." # example: if user send msg like .hello! program will send this to c.ai but if user send hello he will be ignored
 print_user_messages = False
 print_ai_messages = True
 
